15688c17293639ebbf21d2213144bccf528455c7
ederrabelo81-crypto <ederrabelo81@gmail.com>
2026-01-18 21:36:49 -0300

    Update RealEstateList.tsx
    update chip

--- src/pages/RealEstateList.tsx
+++ src/pages/RealEstateList.tsx
@@ -49,11 +49,13 @@ export default function RealEstateList() {
           </div>
           <SearchBar value={query} onChange={setQuery} placeholder="Buscar imóveis..." />
           <div className="flex gap-2 mt-3 overflow-x-auto pb-1 scrollbar-hide">
-            {filters.map((filter) => (  <Chip    key={filter}
-    active={activeFilters.includes(filter)}
-    onClick={() => toggleFilter(filter)}  >
-    {filter}
-  </Chip>
+            {filters.map((filter) => (  <Chip
+  key={filter}
+  active={activeFilters.includes(filter)}
+  onClick={() => toggleFilter(filter)}
+>
+  {filter}
+</Chip>
 ))}
           </div>
         </div>
