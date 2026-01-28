5a3a7a2324baf86b42bbc0edd6e79b615da75c8f
ederrabelo81 <ederrabelo81@gmail.com>
2026-01-28 21:39:26 +0000

    env.local com firebase credentials

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,21 +1,35 @@
-// src/firebase.ts
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
 
-// TODO: Substitua pelas credenciais do seu projeto Firebase
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, signInAnonymously } from "firebase/auth";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBU-fKTA-Rq8JIhY3a6sXe8SfjirxDqwwE",
-  authDomain: "procura-uai.firebaseapp.com",
-  projectId: "procura-uai",
-  storageBucket: "procura-uai.firebasestorage.app",
-  messagingSenderId: "707880233994",
-  appId: "1:707880233994:web:7e705182758246b11b83f3"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
-// Inicializa o Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporta os serviços que você vai usar
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+// Initialize services
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// Function to sign in anonymously
+const signIn = async () => {
+  try {
+    await signInAnonymously(auth);
+    console.log("Signed in anonymously");
+  } catch (error) {
+    console.error("Failed to sign in anonymously. Continuing without authentication.", error);
+  }
+};
+
+signIn();
+
+export { db, auth };
